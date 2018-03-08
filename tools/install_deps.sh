74ff2a4eb6cada504d9a26df931a513752dc6041
LiuLang <gsushzhsosgsu@gmail.com>
2018-03-08 09:21:49 +0800

    Added install-deps
    Change-Id: Ic320cbe408561e76ecfb633ef2f2a7143afa3c4c

---
+++ tools/install_deps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Install dependencies on Debian based x64 systems.
+
+sudo apt-get install \
+  ninja-build \
+  apache2 apache2-bin apache2-data apache2-utils bison bzip2-doc cabextract \
+  cdbs comerr-dev curl debugedit elfutils ffmpeg fonts-beng fonts-beng-extra \
+  fonts-deva fonts-deva-extra fonts-gargi fonts-gubbi fonts-gujr \
+  fonts-gujr-extra fonts-guru fonts-guru-extra fonts-indic fonts-ipafont \
+  fonts-ipafont-gothic fonts-ipafont-mincho fonts-kalapi fonts-knda \
+  fonts-lohit-beng-assamese fonts-lohit-beng-bengali fonts-lohit-gujr \
+  fonts-lohit-guru fonts-lohit-knda fonts-lohit-mlym fonts-lohit-orya \
+  fonts-lohit-taml fonts-lohit-taml-classical fonts-lohit-telu fonts-mlym \
+  fonts-nakula fonts-navilu fonts-orya fonts-orya-extra fonts-pagul \
+  fonts-sahadeva fonts-samyak-deva fonts-samyak-gujr fonts-samyak-mlym \
+  fonts-samyak-taml fonts-sarai fonts-smc fonts-taml fonts-telu \
+  fonts-telu-extra fonts-thai-tlwg fonts-tlwg-garuda fonts-tlwg-garuda-ttf \
+  fonts-tlwg-kinnari fonts-tlwg-kinnari-ttf fonts-tlwg-laksaman \
+  fonts-tlwg-laksaman-ttf fonts-tlwg-loma fonts-tlwg-loma-ttf fonts-tlwg-mono \
+  fonts-tlwg-mono-ttf fonts-tlwg-norasi fonts-tlwg-norasi-ttf \
+  fonts-tlwg-purisa fonts-tlwg-purisa-ttf fonts-tlwg-sawasdee \
+  fonts-tlwg-sawasdee-ttf fonts-tlwg-typewriter fonts-tlwg-typewriter-ttf \
+  fonts-tlwg-typist fonts-tlwg-typist-ttf fonts-tlwg-typo fonts-tlwg-typo-ttf \
+  fonts-tlwg-umpush fonts-tlwg-umpush-ttf fonts-tlwg-waree \
+  fonts-tlwg-waree-ttf gir1.2-atspi-2.0 gir1.2-gconf-2.0 \
+  gir1.2-gnomekeyring-1.0 git-core git-svn gperf krb5-multidev lib32gcc1 \
+  lib32stdc++6 libapache2-mod-php7.0 libapr1 libaprutil1 \
+  libaprutil1-dbd-sqlite3 libaprutil1-ldap libasm1 libasound2-dev \
+  libatk-bridge2.0-dev libatspi2.0-dev libav-tools libbison-dev \
+  libbluetooth-dev libbrlapi-dev libbrlapi0.6 libbz2-dev libc6-i386 libcap-dev \
+  libcups2-dev libcupsfilters-dev libcupsimage2-dev libcurl4-gnutls-dev \
+  libdbus-1-dev libegl1-mesa-dev libepoxy-dev libgbm-dev libgconf2-dev \
+  libgconf2-doc libgles2-mesa-dev libgnome-keyring-dev libgssrpc4 libgtk-3-dev \
+  libjbig-dev libjpeg-dev libjpeg62-turbo-dev libjs-sphinxdoc libjs-underscore \
+  libkadm5clnt-mit11 libkadm5srv-mit11 libkdb5-8 libkrb5-dev liblzma-dev \
+  libnspr4-dev libnss3-dev libopencv-contrib2.4v5 libopencv-legacy2.4v5 \
+  libopencv-ml2.4v5 libopencv-photo2.4v5 libpam0g-dev libpci-dev libpulse-dev \
+  libpython-dev libpython2.7-dev librpmbuild3 librpmsign3 libsctp-dev libsctp1 \
+  libserf-1-1 libspeechd-dev libspeechd2 libsqlite3-dev libssl-dev libssl-doc \
+  libsvn-perl libsvn1 libterm-readkey-perl libtiff5-dev libtiffxx5 \
+  libwayland-bin libwayland-dev libxslt1-dev libxt-dev libyaml-perl php-common \
+  php7.0-cgi php7.0-cli php7.0-common php7.0-json php7.0-opcache \
+  php7.0-readline python-cherrypy3 python-dev python-opencv python-repoze.lru \
+  python-routes python-webob python-yaml python2.7-dev realpath rpm subversion \
+  ttf-dejavu-core ttf-kochi-gothic ttf-kochi-mincho  \
+  wayland-protocols xcompmgr \
+  libgtkglext1-dev
+  
